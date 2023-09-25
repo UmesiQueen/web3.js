96d173180afe89da0fadb7986a49029cb01ba6a8
Queen <queenumesi01@gmail.com>
2023-09-25 19:53:58 +0100

    inspect block, use utils

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Intro to Web3.js · Ethereum Blockchain Developer Crash Course - https://www.dappuniversity.com/articles/web3-js-intro
 import Web3 from "web3";
+import { LegacyTransaction } from "@ethereumjs/tx";
+import { Common, Chain, Hardfork } from "@ethereumjs/common";
+import { hexToBytes, bytesToHex } from "@ethereumjs/util";
+
 import config from "./config.js";
 
 // Configuring the connection to an Ethereum node
 const rpcURL = `https://mainnet.infura.io/v3/${config.infuraPrivateKey}`;
 
-var web3 = new Web3(
+const web3 = new Web3(
   Web3.givenProvider || new Web3.providers.HttpProvider(rpcURL)
 );
 
@@ -272,7 +277,7 @@ function sendTransactionGanache() {
 // sendTransactionGanache();
 
 // `` Using Infura
-var web3 = new Web3(
+const web3_ = new Web3(
   new Web3.providers.HttpProvider(
     `https://sepolia.infura.io/v3/${config.infuraPrivateKey}`
   )
@@ -301,14 +306,259 @@ async function sendTransaction(web3) {
     txObject,
     privateKey1
   );
+  const raw = signedTx.rawTransaction;
+  console.log(raw, "raw transaction");
 
   // Broadcast the transaction
-  web3.eth
-    .sendSignedTransaction(signedTx.rawTransaction)
-    .then((txHash, err) => {
-      console.log("txHash:", txHash);
-    });
+  web3.eth.sendSignedTransaction(raw).then((txHash, err) => {
+    console.log("txHash:", txHash);
+  });
 }
-// sendTransaction(web3);
+// sendTransaction(web3_);
 
 // Sign transactions locally using Ethereumjs/tx ????
+async function signTransactionLocally(web3) {
+  const nonce = await web3.eth.getTransactionCount(address1, "latest");
+  const gasPrice = await web3.eth.getGasPrice();
+
+  const common = Common.custom({
+    chain: Chain.Sepolia,
+    chainId: 11155111,
+    hardfork: Hardfork.Shanghai,
+  });
+
+  // Create transaction data
+  const txData = {
+    nonce: web3.utils.toHex(nonce),
+    to: address2,
+    value: web3.utils.toHex(web3.utils.toWei("0", "ether")),
+    gasLimit: web3.utils.toHex(21000),
+    gasPrice: web3.utils.toHex(gasPrice),
+  };
+
+  // Sign Transaction
+  const tx = LegacyTransaction.fromTxData(txData, {
+    common,
+  });
+  // const signedTx = tx.sign(hexToBytes("0x" + config.privateKey1));
+  const signedTx = tx.sign(privateKey1);
+  const serializedTx = bytesToHex(signedTx.serialize());
+
+  // Broadcast the transaction
+  web3.eth.sendSignedTransaction(serializedTx).then(console.log);
+}
+// signTransactionLocally(web3_);
+
+// ``` Read  deployed smart contract
+// Created and deployed an ERC20 token on Remix IDE  - PEACH TOKEN
+// View on github gist - https://gist.github.com/UmesiQueen/2e138e63c6bf044a1348d15b3396b32f
+const peachContractAddress = "0x4E5BE576de5DCF9fd3215B790b1a7a97CdB27a0C";
+const peachTokenContractABI = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "tokenOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokens",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokens",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "_totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "tokenOwner", type: "address" },
+      { internalType: "address", name: "delegate", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "delegate", type: "address" },
+      { internalType: "uint256", name: "tokens", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "tokenOwner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "receiver", type: "address" },
+      { internalType: "uint256", name: "tokens", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "tokens", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+const peachTokenContract = new web3_.eth.Contract(
+  peachTokenContractABI,
+  peachContractAddress
+);
+
+function peachContractMethods() {
+  peachTokenContract.methods
+    .name()
+    .call()
+    .then((res) => {
+      console.log("Contract Name:", res);
+    });
+
+  peachTokenContract.methods
+    .symbol()
+    .call()
+    .then((res) => {
+      console.log("Contract symbol:", res);
+    });
+
+  peachTokenContract.methods
+    .totalSupply()
+    .call()
+    .then((res) => {
+      console.log("Total Supply:", res);
+    });
+
+  const holderAddress = "0x7f18d720F0D1c78c4510342bEaBa4CBA613c7C81";
+
+  contract.methods
+    .balanceOf(holderAddress)
+    .call()
+    .then((res) => {
+      console.log("Balance:", web3.utils.fromWei(res, "ether"));
+    });
+}
+// peachContractMethods();
+
+// ``` Smart contract Events
+function events() {
+  contract
+    .getPastEvents("allEvents", {
+      fromBlock: 18194692,
+      toBlock: "latest",
+    })
+    .then(console.log);
+}
+// events();
+
+// ``` Inspecting Blocks
+function inspectBlocks() {
+  web3.eth.getBlockNumber().then((res) => console.log(res, "Block Number"));
+
+  web3.eth.getBlock("latest").then((block) =>
+    console.log("Latest: ", {
+      blockNumber: block.number,
+      blockHash: block.hash,
+      blockParentHash: block.parentHash,
+    })
+  );
+
+  web3.eth.getBlockNumber().then((latest) => {
+    for (let i = 0; i < 10; i++) {
+      web3.eth
+        .getBlock(latest - BigInt(i))
+        .then((block) => console.log(`${i}: ${block.number}`));
+    }
+  });
+}
+// inspectBlocks();
+
+// ``` Web3 Utilities
+// Read docs - https://web3js.readthedocs.io/en/v1.2.11/web3-utils.html
+function utils() {
+  web3.eth
+    .getGasPrice()
+    .then((res) => console.log(web3.utils.fromWei(res, "ether")));
+
+  console.log(web3.utils.sha3("Random Stuff")); // string values
+  console.log(web3.utils.soliditySha3("Another random stuff"));
+  console.log(web3.utils.randomHex(8));
+}
+// utils();
